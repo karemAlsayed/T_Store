42e70025ad2c07f038141d1b5b56b791099ce620
mohamedhassan <mohamed7github@gmail.com>
2024-11-29 00:55:27 +0200

    crreate AuthFirebaseServices class

---
+++ lib/features/auth/data/source/auth_firebase_services.dart
@@ -0,0 +1,57 @@
+import 'package:dartz/dartz.dart';
+
+abstract class AuthFirebaseServices {
+  Future<Either> signup();
+  Future<Either> sigin();
+  Future<bool> isLoggedIn();
+  Future<Either> logout();
+  Future<Either> resetPassword({required String email});
+  Future<Either> sendEmailVerification();
+  Future<bool> isVerifiedEmail();
+}
+
+
+class AuthFirebaseServicesImpl extends AuthFirebaseServices{
+  @override
+  Future<bool> isLoggedIn() {
+    // TODO: implement isLoggedIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isVerifiedEmail() {
+    // TODO: implement isVerifiedEmail
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either> resetPassword({required String email}) {
+    // TODO: implement resetPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either> sigin() {
+    // TODO: implement sigin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+
+}
