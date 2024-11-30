517999a1c23a64abe38ebb0a71a7afa485281339
karemAlsayed <krym86756@gmail.com>
2024-11-30 19:53:15 +0200

    edit

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -4,37 +4,37 @@ import 'package:store_app/features/auth/domain/repositories/auth/auth_repository
 class AuthRepositoryImpl extends AuthRepository {
   @override
   Future<bool> isLoggedIn() {
-    // TODO: implement isLoggedIn
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> logout() {
-    // TODO: implement logout
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> resetPassword({required String email}) {
-    // TODO: implement resetPassword
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> sendEmailVerification() {
-    // TODO: implement sendEmailVerification
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> signIn() {
-    // TODO: implement signIn
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> signup() {
-    // TODO: implement signup
+    
     throw UnimplementedError();
   }
 }
--- lib/features/auth/data/source/auth_firebase_services.dart
+++ lib/features/auth/data/source/auth_firebase_services.dart
@@ -12,37 +12,37 @@ abstract class AuthFirebaseServices {
 class AuthFirebaseServicesImpl extends AuthFirebaseServices {
   @override
   Future<bool> isLoggedIn() {
-    // TODO: implement isLoggedIn
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> logout() {
-    // TODO: implement logout
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> resetPassword({required String email}) {
-    // TODO: implement resetPassword
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> sendEmailVerification() {
-    // TODO: implement sendEmailVerification
+  
     throw UnimplementedError();
   }
 
   @override
   Future<Either> sigin() {
-    // TODO: implement sigin
+    
     throw UnimplementedError();
   }
 
   @override
   Future<Either> signup() {
-    // TODO: implement signup
+  
     throw UnimplementedError();
   }
 }
